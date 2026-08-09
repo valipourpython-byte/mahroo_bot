b2f5ea93241b80d7c5b86a6951052cea2c0aef13
valipourpython-byte <valipourpython@gmail.com>
2026-08-09 08:32:24 +0330

    Update app.py

--- app.py
+++ app.py
@@ -31,5 +31,8 @@ def receive_message():
 
     return jsonify({"status": "ok"})
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
